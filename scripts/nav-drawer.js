30f089a8f93bcbe004868e525f9bc6db782a118f
unknown <gfsteinmetz@gmail.com>
2026-09-01 16:26:11 -0300

    correção do menu hamburguer + botão de fechar estilizado

--- scripts/nav-drawer.js
+++ scripts/nav-drawer.js
@@ -1,4 +1,10 @@
 function toggleMenu() {
     const menu = document.getElementById("nav-drawer-menu")
+    const menuIcon = document.getElementById("menu-icon")
+    
     menu.classList.toggle("menu-aberto")
+    menu.classList.toggle("menu-fechado")
+
+    menuIcon.classList.toggle("fa-bars")
+    menuIcon.classList.toggle("fa-xmark")
 }
