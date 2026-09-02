c84d3143f21f25f95b7779181c2a5344a303b9a7
unknown <gabriel.steinmetz@escola.pr.gov.br>
2026-09-01 21:35:27 -0300

    adicionado o servidor via node com express e mysql2 e script movido para public

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 8000
+
+// Middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Define a pasta public como estática
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+// Inicialização do servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
