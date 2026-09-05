07992f6433a8a8b7fd5f42d6ea5b7d615ab7706a
unknown <gfsteinmetz@gmail.com>
2026-09-05 18:02:54 -0300

    login do admin adicionado + função de buscarPorEmail no userModel

--- controller/admController.js
+++ controller/admController.js
@@ -1,14 +1,60 @@
-const admModel = require('../model/userModel')
+const bcrypt = require("bcrypt")
+const userModel = require("../model/userModel")
 
-function loginAdm(req, res) {
-    admModel.loginAdm(req.body, (erro) => {
-        if (erro) {
-            console.log(erro)
-            return res.send('Erro ao fazer login.')
-        }
-        res.send("")
-    })
-}
+async function loginAdm(req, res) {
+
+        const email = req.body.email;
+        const senha = req.body.senha;
+
+        const user = await userModel.buscarPorEmail(email, async (erro, user) => {
+            // erro no banco
+            if (erro) {
+                console.log(erro);
+
+                return res.status(500).json({
+                    erro: "Erro ao buscar usuário"
+                });
+            }
+
+            // se usuario não existir
+            if (!user) {
+                return res.status(401).json({
+                    erro: "Email ou senha incorretos1"
+                })
+            }
+
+            try {
+                const senhaCorreta = await bcrypt.compare(senha, user.user_pass)
+                
+                
+                if (!senhaCorreta) {
+                    // se a senha não coincidir
+                    return res.status(401).json({
+                        erro: "Email ou senha incorretos2"
+                    })
+                }
+            
+                if (user.user_tipo !== 'admin') {
+                    // se não for admin
+                    return res.status(403).json({
+                        erro: "Você não é um administrador"
+                    })
+                }
+
+                // se passar por todas as verificações
+                return res.status(200).json({
+                    mensagem: "Login feito com sucesso"
+                })
+            } catch (erro) {
+                console.log(erro)
+                
+                return res.status(500).json({
+                    erro: "Erro ao verificar senha"
+                });
+            }
+        })
+    } 
+    
 
 module.exports = {
     loginAdm
--- model/userModel.js
+++ model/userModel.js
@@ -1,21 +1,51 @@
+const bcrypt = require('bcrypt')
 const conexao = require('../config/database')
 
-function criarUsuario(user, callback) {
-    const sql = `
-        INSERT INTO tb_usuarios
-        (user_name, user_email, user_telefone, user_pass, user_avatar)
-        VALUES (?, ?, ?, ?, ?)
-    `
+async function criarUsuario(user, callback) {
 
-    conexao.query(sql, [
-        user.nome,
-        user.email,
-        user.telefone,
-        user.senha,
-        user.avatar
-    ], callback)
+    try {
+
+        const senhaHash = await bcrypt.hash(user.senha, 10);
+
+        const sql = `
+            INSERT INTO tb_usuarios
+            (user_name, user_email, user_telefone, user_pass, user_avatar)
+            VALUES (?, ?, ?, ?, ?)
+        `
+
+        conexao.query(sql, [
+            user.nome,
+            user.email,
+            user.telefone,
+            senhaHash,
+            user.avatar
+        ], callback)
+    
+    } catch (erro) {
+        callback(erro);
+    }
 }
 
+function buscarPorEmail(email, callback) {
+    
+    const sql = `
+        SELECT * FROM tb_usuarios
+        WHERE user_email = ?
+    `
+
+    conexao.query(sql, [email], (erro, usuarios) => {
+        if (erro) {
+            return callback(erro);
+        }
+
+        return callback(null, usuarios[0])
+    })
+}
+
+
+
+
 module.exports = {
-    criarUsuario
+    criarUsuario,
+    buscarPorEmail
 }
