7119aa3c27b9b52a5ae677c3778e1a5fdcc99924
unknown <gfsteinmetz@gmail.com>
2026-09-19 15:18:17 -0300

    sistema anti hussein (limitador nas rotas de comentario e cadastro)

--- routes/comentarioRoutes.js
+++ routes/comentarioRoutes.js
@@ -1,9 +1,30 @@
 const express = require("express")
 const router = express.Router()
 
+const { rateLimit } = require("express-rate-limit")
+
 const comentarioController = require("../controller/comentarioController")
 
-router.post("/", comentarioController.salvarComentario)
+const limitarComentarios = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 5,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+
+    skip: (req) => {
+        return !req.session.usuario
+    },
+
+    keyGenerator: (req) => {
+        return String(req.session.usuario.id)
+    },
+
+    message: {
+        erro: "Você enviou muitas solicitações. Aguarde um minuto antes de comentar novamente."
+    }
+})
+
+router.post("/", limitarComentarios, comentarioController.salvarComentario)
 router.delete("/:id", comentarioController.excluirComentario)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,16 @@ const express = require('express')
 const router = express.Router()
 const { rateLimit } = require("express-rate-limit")
 
+const limitarCadastro = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    limit: 10,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    message: {
+        erro: "Muitas tentativas de cadastro. Aguarde 15 minutos antes de tentar novamente."
+    }
+})
+
 const limitarEdicaoPerfil = rateLimit({
     windowMs: 15 * 60 * 1000,
     limit: 10,
@@ -14,11 +24,11 @@ const limitarEdicaoPerfil = rateLimit({
 
 const userController = require('../controller/userController')
 
-router.post('/cadastro', userController.criarUsuario)
+router.post('/cadastro', limitarCadastro, userController.criarUsuario)
 router.post('/login', userController.loginUsuario)
 router.get('/sessao', userController.verificarSessao)
 router.post('/logout', userController.logoutUsuario)
-router.get("/perfil", userController.buscarPerfil)
-router.post("/perfil", limitarEdicaoPerfil, userController.atualizarPerfil)
+router.get('/perfil', userController.buscarPerfil)
+router.post('/perfil', limitarEdicaoPerfil, userController.atualizarPerfil)
 
 module.exports = router
